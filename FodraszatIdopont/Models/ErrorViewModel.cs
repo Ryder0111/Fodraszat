6e704b7c5ebe8a02a5e5aea88236d561957306c5
Bottyán Vak <ecdl01@vbjnet.hu>
2025-12-15 13:50:02 +0100

    Feltöltés

---
+++ FodraszatIdopont/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FodraszatIdopont.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
