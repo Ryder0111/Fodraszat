6423228cb6127ff81a7047e6082d1208d3f737a7
Bottyán Vak <ecdl01@vbjnet.hu>
2026-02-05 12:21:04 +0100

    Service/Hairdresser class

---
+++ FodraszatIdopont/Models/Hairdresser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Build.Framework;
+
+namespace FodraszatIdopont.Models
+{
+    public class Hairdresser
+    {
+        public string HairdresserId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
