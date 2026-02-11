858e7260e84ae70c6fa0a761cae10ebd6d10168c
Bottyán Vak <ecdl01@vbjnet.hu>
2026-02-11 10:47:08 +0100

    Ctor, error msg

--- FodraszatIdopont/Models/User.cs
+++ FodraszatIdopont/Models/User.cs
@@ -5,8 +5,14 @@ namespace FodraszatIdopont.Models
     public class User
     {
         public int UserId { get; set; }
+
+        [Required(ErrorMessage = "A név megadása kötelező!")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Az email megadása kötelező!")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "A jelszó megadása kötelező!")]
         public string PasswordHash { get; set; }
         public string Sex { get; set; }
 
