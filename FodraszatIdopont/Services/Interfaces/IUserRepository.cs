81d9b682ead65bda9ee283d22e61ac967829ce4c
Marcell Belák <marcellbelak@gmail.com>
2026-02-11 21:15:58 +0100

    IUserRepository / UserRepository

--- FodraszatIdopont/Services/Interfaces/IUserRepository.cs
+++ FodraszatIdopont/Services/Interfaces/IUserRepository.cs
@@ -1,6 +1,20 @@
-﻿namespace FodraszatIdopont.Services.Interfaces
+﻿using FodraszatIdopont.Models.Entities;
+
+namespace FodraszatIdopont.Services.Interfaces
 {
     public interface IUserRepository
     {
+        Task<List<User>> GetAll();
+
+        Task<User?> GetUserByEamil(string eamil);
+
+        Task<User?> GetById(int id);
+
+        Task Add(User felhasznalo);
+
+        Task Delete(User felhasznalo);
+
+        Task Update(User felhasznalo);
+
     }
 }
---
+++ FodraszatIdopont/Services/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using FodraszatIdopont.Models.Entities;
+using FodraszatIdopont.Services.Interfaces;
+
+namespace FodraszatIdopont.Services
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task Add(User felhasznalo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(User felhasznalo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User?> GetUserByEamil(string eamil)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(User felhasznalo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
