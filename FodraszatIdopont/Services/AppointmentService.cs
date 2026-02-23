cf0c8baf979728eee346be590a5ce1d3ea60c88d
Ryder0111 <gabornagy428@gmail.com>
2026-02-23 19:20:19 +0100

    Cancel elseif

--- FodraszatIdopont/Services/AppointmentService.cs
+++ FodraszatIdopont/Services/AppointmentService.cs
@@ -20,23 +20,27 @@ namespace FodraszatIdopont.Services
         {
             var idopont = await _Appointmentrepo.GetById(appointment.AppointmentId);
 
-            if(idopont == null)
+            if (idopont == null)
             {
                 return Results<Appointment>.Fail("Nincs ilyen időpontfoglalás!");
-            }
-            
-            if(idopont.AppointmentStatus == Models.Enums.AppointmentStatus.Cancelled)
-            {
-                return Results<Appointment>.Fail("Ez az időpontfoglalás már le van mondva");
+
             }
 
-            if (DateTime.Now.AddDays(1) > idopont.StartTime)
+            else if (idopont.AppointmentStatus == Models.Enums.AppointmentStatus.Cancelled)
+            {
+                return Results<Appointment>.Fail("Ez az időpontfoglalás már le van mondva");
+
+            }
+
+            else if (DateTime.Now.AddDays(1) > idopont.StartTime)
             {
                 return Results<Appointment>.Fail("Ezt az időpontot már nem lehet lemondani.");
             }
-
-            await _Appointmentrepo.Update(idopont);
-            return Results<Appointment>.Ok(idopont);
+            else
+            {
+                await _Appointmentrepo.Update(idopont);
+                return Results<Appointment>.Ok(idopont);
+            }
         }
 
         public async Task<Results<Appointment>> CreateAppointment(Appointment appointment, int ServiceId)
