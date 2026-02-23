c6f4840ce53686d1b7fb01902f8b4321cc262c58
Marcell Belák <marcellbelak@gmail.com>
2026-02-23 16:12:49 +0100

    AppointmentRepository

--- FodraszatIdopont/Repositories/AppointmentService.cs
+++ FodraszatIdopont/Repositories/AppointmentService.cs
@@ -1,48 +1,68 @@
-﻿using FodraszatIdopont.Models.Entities;
+﻿using FodraszatIdopont.Data;
+using FodraszatIdopont.Models.Entities;
 using FodraszatIdopont.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FodraszatIdopont.Repositories
 {
-    public class AppointmentService : IAppointmentRepository
+    public class AppointmentRepository : IAppointmentRepository
     {
-        public Task<Appointment> Creat(Appointment appointment)
+        private readonly BarberDbContext _db;
+
+        public AppointmentRepository(BarberDbContext db)
         {
-            throw new NotImplementedException();
+            _db = db;
         }
 
-        public Task<List<Appointment>> GetAppointmentsByDateAndHairdresser(int id, DateOnly date)
+        public async Task<Appointment> Create(Appointment appointment)
         {
-            throw new NotImplementedException();
+            _db.Appointments.Add(appointment);
+            await _db.SaveChangesAsync();
+            return appointment;
         }
 
-        public Task<List<Appointment>> GetAppointmentsByHairdresserInTimeRange(int id, DateTime start, DateTime end)
+        public async Task<bool> ExistsInTimeRange(int id, DateTime start, DateTime end)
         {
-            throw new NotImplementedException();
+            return await _db.Appointments.Where(h => h.HairdresserId == id).AnyAsync(a => start < a.EndTime && end > a.StartTime);
         }
 
-        public Task<List<Appointment>> GetByHairdresserId(int id)
+
+        public async Task<List<Appointment>> GetAppointmentsByDateAndHairdresser(int id, DateOnly date)
         {
-            throw new NotImplementedException();
+            return await _db.Appointments.Where(h => h.HairdresserId == id && h.StartTime >= date.ToDateTime(TimeOnly.MinValue) && h.StartTime < date.ToDateTime(TimeOnly.MinValue).AddDays(1)).ToListAsync();
         }
 
-        public Task<Appointment> GetById(int id)
+        public async Task<List<Appointment>> GetAppointmentsByHairdresserInTimeRange(int id, DateTime start, DateTime end)
         {
-            throw new NotImplementedException();
+            return await _db.Appointments.Where(a => a.HairdresserId == id && start < a.EndTime && end > a.StartTime).ToListAsync();
         }
 
-        public Task<List<Appointment>> GetByUserId(int id)
+
+        public async Task<List<Appointment>> GetByHairdresserId(int id)
         {
-            throw new NotImplementedException();
+            return await _db.Appointments.Where(h => h.HairdresserId == id).ToListAsync();
         }
 
-        public Task<List<Appointment>> GetFutureAppointmentsByUser(int id)
+        public async Task<Appointment?> GetById(int id)
         {
-            throw new NotImplementedException();
+            return await _db.Appointments.SingleOrDefaultAsync(a => a.AppointmentId == id);
         }
 
-        public Task<Appointment> Update(Appointment appointment)
+        public async Task<List<Appointment>> GetByUserId(int id)
         {
-            throw new NotImplementedException();
+            return await _db.Appointments.Where(u => u.UserId == id).ToListAsync();
+        }
+
+        public async Task<List<Appointment>> GetFutureAppointmentsByUser(int id)
+        {
+            return await _db.Appointments.Where(u => u.UserId == id && u.StartTime > DateTime.Now).ToListAsync();
+        }
+
+        public async Task Update(Appointment appointment)
+        {
+            _db.Appointments.Update(appointment);
+            await _db.SaveChangesAsync();
         }
     }
 }
