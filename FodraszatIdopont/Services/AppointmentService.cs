ba65920750c7ab9d3eca59ba6ddb86df1606f9a5
Marcell Belák <marcellbelak@gmail.com>
2026-02-23 19:30:51 +0100

    }

--- FodraszatIdopont/Services/AppointmentService.cs
+++ FodraszatIdopont/Services/AppointmentService.cs
@@ -66,6 +66,7 @@ namespace FodraszatIdopont.Services
         public Task<Results<List<Appointment>>> GetHairdresseSchedule(Hairdresser hairdresser)
         {
             throw new NotImplementedException();
+        }
 
         public async Task<Results<List<Appointment>>> GetUserAppointments(User user)
         {
