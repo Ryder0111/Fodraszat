bb84f67251721d00aaa0f7d3340a15ebf64d6e88
Bottyán Vak <ecdl01@vbjnet.hu>
2026-03-03 09:36:20 +0100

    AdminServiceTest

---
+++ UnitTests/AdminServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using FodraszatIdopont.Models.Entities;
+using FodraszatIdopont.Repositories.Interfaces;
+using FodraszatIdopont.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    internal class AdminServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var _mockRepo = new Mock<IServiceRepository>();
+            var _service = new AdminService(_mockRepo.Object);
+        }
+    }
+}
