bfbbd050a9c4e4441977a41d2d03473143e4ac1a
Bottyán Vak <ecdl01@vbjnet.hu>
2026-03-09 14:02:29 +0100

    ClosedDayService

---
+++ FodraszatIdopont/Services/ClosedDayservice.cs
@@ -0,0 +1,37 @@
+﻿using FodraszatIdopont.Helpers;
+using FodraszatIdopont.Models.Entities;
+using FodraszatIdopont.Repositories.Interfaces;
+using FodraszatIdopont.Services.Interface;
+
+namespace FodraszatIdopont.Services
+{
+    public class ClosedDayService : IClosedDayService
+    {
+        private readonly IClosedDayRepository _repo;
+
+        public ClosedDayService(IClosedDayRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public Task<Results<ClosedDay>> AddClosedDay(ClosedDay closedday)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Results<List<ClosedDay>>> GetClosedDays()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Results<bool>> IsClosedDay(DateOnly closedday)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Results<ClosedDay>> RemoveClosedDay(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
