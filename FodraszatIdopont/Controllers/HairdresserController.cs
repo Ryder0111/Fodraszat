77b8da24e7866d17d625ddcedae5f9d5361bbeff
Bottyán Vak <ecdl01@vbjnet.hu>
2026-03-10 09:37:17 +0100

    safe

---
+++ FodraszatIdopont/Controllers/HairdresserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FodraszatIdopont.Controllers
+{
+    public class HairdresserController : Controller
+    {
+        [Authorize(Roles = "Hairdresser")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
