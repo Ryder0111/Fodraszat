88b21a97c8a6c6a42b648ee9a0d81f834a18ba92
Bottyán Vak <ecdl01@vbjnet.hu>
2026-03-26 12:01:37 +0100

    idopontjaim

---
+++ FodraszatIdopont/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FodraszatIdopont.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
